5801e98879675f9931a80a4cefe3a04203e0c33b
Colbyn Wadman <hello@colbyn.com>
2025-11-01 17:48:44 -0700

    commit initial codebase

---
+++ run.sh
@@ -0,0 +1,10 @@
+set -e
+
+touch fragments/about.html
+touch fragments/contact.html
+touch fragments/footer.html
+touch fragments/gallery.html
+touch fragments/hero.html
+touch fragments/service-area.html
+touch fragments/services.html
+touch fragments/testimonials.html
---
+++ scripts/open.sh
@@ -0,0 +1,5 @@
+set -e
+
+'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --app='http://127.0.0.1:8002'
+
+# '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --app='http://127.0.0.1:8002' --auto-open-devtools-for-tabs
